d241316858c805c81d549960bdf4891b68775d25
jamesbradleym <jamesbradleym@gmail.com>
2024-11-13 10:45:49 -0500

    Manage status effect by source
    Co-authored-by: jamesbradleym <jamesbradleym@gmail.com>
    Co-authored-by: TracentEden <92269743+TracentEden@users.noreply.github.com>

--- scripts/specs/core/CBaseEntity.lua
+++ scripts/specs/core/CBaseEntity.lua
@@ -888,6 +888,12 @@ function CBaseEntity:getEquippedItem(slot)
 end
 
 ---@nodiscard
+---@param equipmentID integer
+---@return boolean
+function CBaseEntity:hasEquipped(equipmentID)
+end
+
+---@nodiscard
 ---@param itemID integer
 ---@param location integer?
 ---@return boolean
@@ -2715,8 +2721,10 @@ end
 ---@param subType integer?
 ---@param subPower integer?
 ---@param tier integer?
+---@param SourceType integer?
+---@param SourceTypeParam integer?
 ---@return boolean
-function CBaseEntity:addStatusEffect(effectID, power, tick, duration, subType, subPower, tier)
+function CBaseEntity:addStatusEffect(effectID, power, tick, duration, subType, subPower, tier, SourceType, SourceTypeParam)
 end
 
 ---@param effect CStatusEffect
@@ -2770,6 +2778,14 @@ function CBaseEntity:getStatusEffect(StatusID, SubType)
 end
 
 ---@nodiscard
+---@param StatusID integer
+---@param SourceType integer
+---@param SourceTypeParam integer
+---@return CStatusEffect?
+function CBaseEntity:getStatusEffectBySource(StatusID, SourceType, SourceTypeParam)
+end
+
+---@nodiscard
 ---@return table
 function CBaseEntity:getStatusEffects()
 end
@@ -2813,8 +2829,10 @@ end
 
 ---@param StatusID integer
 ---@param SubType integer?
+---@param SourceType integer?
+---@param SourceTypeParam integer?
 ---@return boolean
-function CBaseEntity:delStatusEffect(StatusID, SubType)
+function CBaseEntity:delStatusEffect(StatusID, SubType, SourceType, SourceTypeParam)
 end
 
 ---@param flag integer
