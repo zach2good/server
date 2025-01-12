23162f5c6346da4f8937bc904e97dba480dee2c7
claywar <claywar@lsb.dev>
2025-01-12 08:34:12 -0500

    [client] January 2025 Version, Titles, Text ID shifts

--- scripts/zones/Halvung/IDs.lua
+++ scripts/zones/Halvung/IDs.lua
@@ -21,14 +21,14 @@ zones[xi.zone.HALVUNG] =
         THE_PARTY_WILL_BE_REMOVED     = 7803, -- If all party members' HP are still zero after # minute[/s], the party will be removed from the battlefield.
         WIDE_TRENCH                   = 7923, -- There is a wide trench around the gate here. There are three keyholes of differing sizes inside the trench.
         MINING_IS_POSSIBLE_HERE       = 7946, -- Mining is possible here if you have <item>.
-        BLUE_FLAMES                   = 7985, -- You can see blue flames flickering from a hole in the ground here...
-        SICKLY_SWEET                  = 8037, -- A sickly sweet fragrance pervades the air...
-        THIN_LAYER_OF_CINDER          = 8044, -- The ground is carpeted in a thin layer of cinder.
-        DRAWS_NEAR                    = 8059, -- Something draws near!
-        DULL_PIECE                    = 8060, -- A dull piece of metal lies on the ground. It appears to be a bracelet of sorts, but the layers of grime covering its surface render it wholly unwearable.
-        LIFT_LEVER                    = 8063, -- You lift the lever with all your might!
-        UNITY_WANTED_BATTLE_INTERACT  = 8102, -- Those who have accepted % must pay # Unity accolades to participate. The content for this Wanted battle is #. [Ready to begin?/You do not have the appropriate object set, so your rewards will be limited.]
-        COMMON_SENSE_SURVIVAL         = 8124, -- It appears that you have arrived at a new survival guide provided by the Adventurers' Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
+        BLUE_FLAMES                   = 7986, -- You can see blue flames flickering from a hole in the ground here...
+        SICKLY_SWEET                  = 8038, -- A sickly sweet fragrance pervades the air...
+        THIN_LAYER_OF_CINDER          = 8045, -- The ground is carpeted in a thin layer of cinder.
+        DRAWS_NEAR                    = 8060, -- Something draws near!
+        DULL_PIECE                    = 8061, -- A dull piece of metal lies on the ground. It appears to be a bracelet of sorts, but the layers of grime covering its surface render it wholly unwearable.
+        LIFT_LEVER                    = 8064, -- You lift the lever with all your might!
+        UNITY_WANTED_BATTLE_INTERACT  = 8103, -- Those who have accepted % must pay # Unity accolades to participate. The content for this Wanted battle is #. [Ready to begin?/You do not have the appropriate object set, so your rewards will be limited.]
+        COMMON_SENSE_SURVIVAL         = 8125, -- It appears that you have arrived at a new survival guide provided by the Adventurers' Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
     },
     mob =
     {
